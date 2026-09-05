63ac31701c81ba1577124be0560c4769a3e5a838
kasperhagens <92027840+kasperhagens@users.noreply.github.com>
2026-09-05 14:03:50 +0200

    Update substitutionSumtwothree.m

--- Octave/substitutionSumtwothree.m
+++ Octave/substitutionSumtwothree.m
@@ -1,6 +1,3 @@
-## Author: Kasper Hagens <kasperhagens@kaspers-air.home>
-## Created: 2022-12-23
-
 %Substitution CHI(y, z', z) = (y-1, z'+y, z+y)
 %or equivalently
 %CHI(Y(1), Y(2), Y(3)) = (Y(1)-1, Y(2)+Y(1), Y(3)+Y(1))
